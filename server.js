75cb95580cdf3ff6f63a418d49cf7e527c7f830d
JPB <jbanas9124@gmail.com>
2021-04-21 01:36:12 -0500

    post and get functionality, allows user to place new animal in Array

--- server.js
+++ server.js
@@ -7,6 +7,18 @@ const PORT = process.env.PORT || 3001;
 //instantiate express
 const app = express();
 
+//data is only read by require('express'), it doesn't change it
+//so we have to push new data to an array -from app.post() to createNewAnimal()
+//then use fs to write to animals.json in the /data/ folder
+const fs = require('fs');
+//path works with fs to create a more predictable file path, especially when working with heroku
+const path = require('path');
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
 //require animals.json
 const { animals } = require('./data/animals');
 
@@ -56,6 +68,42 @@ function findById(id, animalsArray) {
   return result;
 }
 
+//validate new animal object
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== 'string') {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== 'string') {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== 'string') {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
+
+// function arguments passed from app.post
+function createNewAnimal(body, animalsArray) {
+  // console.log(body);
+  // our function's main code will go here!
+  const animal = body;
+  animalsArray.push(animal);
+
+  //update to write to animals.json
+  fs.writeFileSync(
+    path.join(__dirname, './data/animals.json'),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+
+
+  // return finished code to post route for response
+  // return body;
+  return animal;
+}
+
 //api request/response
 app.get('/api/animals', (req, res) => {
   let results = animals;
@@ -72,6 +120,27 @@ app.get('/api/animals/:id', (req, res) => {
     res.json(result);
   } else {
     res.sendStatus(404);
+  }
+});
+
+// post request for users request the server to accept data
+app.post('/api/animals', (req, res) => {
+  // // req.body is where our incoming content will be
+  // console.log(req.body);
+
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString()
+
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+
+    // add animal to json file and animals array in this function
+    const animal = createNewAnimal(req.body, animals);
+
+    // res.json(req.body);
+    res.json(animal);
   }
 });
 
